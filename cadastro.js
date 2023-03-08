971e55f65fd319a0bd0f2ed7d4e52bd8be2b03f5
Mattheus <mattheusgabriel668@gmail.com>
2023-03-08 00:10:28 -0300

    adicionando funcionalidade de mostrar a mensagem

--- cadastro.js
+++ cadastro.js
@@ -1,3 +1,6 @@
 function signeUp(email)
 return email 
+function mostraMensagem(email)
+return "bem vindo ao sistema" 
 //pegando o email e cadastrando na plataforma
+//adicionando funcao de mensagem
